e090ec021768bd7f32705b8ba5691156a46d47a5
Karel Kočí <karel.koci@nic.cz>
2020-06-16 17:25:31 +0200

    diagnostics.sh: improve arguments parsing
    We can easilly use getopts to parse arguments instead of relying on
    exact arguments order.

--- diagnostics.sh
+++ diagnostics.sh
@@ -28,8 +28,14 @@ module_help() {
 	done
 }
 
+print_usage() {
+	echo "Usage: $(basename $0) [-b] [-o <file> | -O <file>] [module1[ module2[...]]]"
+}
+
 print_help() {
-	echo "$(basename $0) [-b] [-o <file> | -O <file>] [module1[ module2[...]]]"
+	print_usage
+	echo
+	echo "Arguments:"
 	echo "	-b		$(gettext "run in background")"
 	echo "	-o <file>	$(gettext "print output to a file")"
 	echo "	-O <file>	$(gettext "print output to a directory module per file")"
@@ -76,33 +82,55 @@ module_wrapper() {
 }
 
 
-if [ "$1" = "-b" ] ; then
-	shift
-	"$0" "$@" >/dev/null 2>&1 &
+OUTPUT_FILE=
+OUTPUT_DIRECTORY=
+background="n"
+
+while getopts "bBo:O:" opt; do
+	case "$opt" in
+		b)
+			background="y"
+			;;
+		o)
+			OUTPUT_FILE="$OPTARG"
+			;;
+		O)
+			OUTPUT_DIRECTORY="$OPTARG"
+			;;
+		*)
+			print_usage
+			exit 1
+	;;
+	esac
+done
+shift "$((OPTIND - 1))"
+
+if [ "$background" = "y" -a "$TURRIS_DIAGNOSTICS_IN_BACKGROUND" != "1" ]; then
+	export TURRIS_DIAGNOSTICS_IN_BACKGROUND=1
+	"$0" "$@" >/dev/null 2>&1 </dev/null &
 	exit 0
 fi
 
-if [ "$1" = "-o" ] ; then
-	shift
-	OUTPUT_FILE="$1"
-	# clean the last output files
+# Clean the last output files
+if [ -n "$OUTPUT_FILE" ]; then
 	rm -rf "$OUTPUT_FILE"
 	rm -rf "$OUTPUT_FILE".preparing
-	shift
-fi
-
-if [ "$1" = "-O" ] ; then
-	shift
-	OUTPUT_DIRECTORY="$1"
+elif [ -n "$OUTPUT_DIRECTORY" ]; then
 	rm -rf "$OUTPUT_DIRECTORY" "$OUTPUT_DIRECTORY.preparing"
-	mkdir -p "$OUTPUT_DIRECTORY.preparing" || ( echo "Failed to created the log directory" && exit 1 )
-	shift
+	mkdir -p "$OUTPUT_DIRECTORY.preparing" || {
+		echo "Failed to created the log directory" >&2
+		exit 1
+	}
 fi
 
-if [ "$1" = help ] ; then
-	print_help
-	exit 0
-fi
+# Print help if requested (this should ignore any module execution)
+for arg in "$@"; do
+	if [ "$arg" = "help" ]; then
+		print_help
+		exit 0
+	fi
+done
+
 
 if [ $# = 0 ] ; then
 	# no parameters run all modules
